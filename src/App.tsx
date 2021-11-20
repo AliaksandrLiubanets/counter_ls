b8ee16d790ec77f459bfbbf08663aac08e8788dc
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-11-20 11:06:04 +0300

    Add style for inc and reset buttons:
    When number is equal to maxValue inc button disabled and cursor gets no
    pointer.
    When number is equel to minValue reset button gets disabled and cursor
    gets no pointer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import DisplayCounter from './components/DisplayCounter'
 import DisplaySettings from './components/DisplaySettings'
 
 function App() {
+
+    // const [maxNumber, setMaxNumber] = useState(maxValue)
+    localStorage.setItem('maxValue', '5')
+    const maxValueStr = localStorage.getItem('maxValue')
+    if (maxValueStr) {
+        const maxValue = JSON.parse(maxValueStr)
+    }
+
+
     return (
         <div className="App">
             <DisplaySettings/>
-            <DisplayCounter/>
+            <DisplayCounter maxValue={5} minValue={0}/>
         </div>
     )
 }
--- src/components/DisplayCounter.tsx
+++ src/components/DisplayCounter.tsx
@@ -2,18 +2,27 @@ import s from '../styles/styles.module.css'
 import {useState} from 'react'
 
 type DisplayCounterPropsType = {
-
+    maxValue: number
+    minValue: number
 }
 
 function DisplayCounter(props: DisplayCounterPropsType) {
 
     const [number, setNumber] = useState(0)
     const Increment = () => {
-        setNumber(number + 1)
+        if (number < props.maxValue) {
+            setNumber(number + 1)
+        }
+
     }
     const Reset = () => {
         setNumber(0)
     }
+
+    const incStyle = `${number === props.maxValue ? s.window__small__buttons_inc : undefined}`
+    const resetStyle = `${number === props.minValue ? s.window__small__buttons_reset : undefined}`
+    const disableInc = number === props.maxValue ? true : false
+    const disableReset = number === props.minValue ? true : false
 
 
     return <div className={s.window__frame}>
@@ -25,10 +34,9 @@ function DisplayCounter(props: DisplayCounterPropsType) {
         </div>
         <div className={s.window__small}>
             <div className={s.window__small__buttons}>
-                <div><button onClick={Increment}>inc</button></div>
-                <div><button onClick={Reset}>reset</button></div>
+                <div ><button className={incStyle} disabled={disableInc} onClick={Increment}>inc</button></div>
+                <div><button className={resetStyle} disabled={disableReset} onClick={Reset}>reset</button></div>
             </div>
-
         </div>
     </div>
 }
