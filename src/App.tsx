5021b296fb9c2ece4a817cfc78f364bb0aabe441
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-11-20 07:41:09 +0300

    Add styles for Window component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Window from './components/Window'
 
 function App() {
   return (
     <div className="App">
-  
+      <Window />
+      <Window />
     </div>
   );
 }
